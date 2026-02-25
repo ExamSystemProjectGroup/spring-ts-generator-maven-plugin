c3c823862bd4217dabcf4235cb4c6c8429189798
summerain0 <2351602624@qq.com>
2026-02-25 22:35:18 +0800

    预定义空mojo

---
+++ src/main/java/club/summerain0/maven/plugin/tsgen/mojo/GenerateMojo.java
@@ -0,0 +1,17 @@
+package club.summerain0.maven.plugin.tsgen.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * 生成ts结构
+ *
+ * @author summerain0
+ */
+@Mojo(name = "generate")
+public class GenerateMojo extends AbstractMojo {
+    @Override
+    public void execute() {
+
+    }
+}
